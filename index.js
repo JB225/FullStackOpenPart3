8b44ac63c8d7ccd010fb54f1bb73fd8f4f865b9f
Jacob <jbale444@outlook.com>
2023-08-19 09:59:57 +0100

    Started to add functionality for an API to access details of a specific person for exercise 3.3

--- index.js
+++ index.js
@@ -29,6 +29,26 @@ app.get('/api/persons', (request, response) => {
     response.json(phoneNumbers)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = parseInt(request.params.id)
+  console.log(phoneNumbers.entries)
+  const person = phoneNumbers.find(person => {
+    person.id === id
+    console.log(person.id === id)
+    // console.log("Person: " + person.id)
+    // console.log("ID: " + id)
+    // console.log(person.id == id)
+    // console.log(typeof(id))
+  })
+  console.log(person)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 app.get('/info', (request, response) => {
     response.send(`
         <p>Phonebook has info for ${phoneNumbers.length}</p>
