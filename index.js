93b699b87b97ac4aa786e63c1f4855f5dca2798b
Jacob <jbale444@outlook.com>
2023-08-20 11:07:59 +0100

    Added method to allow user to add new people to the phonebook via a REST API and delete people for exercise 3.4/3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+
 app.set('json spaces', 2)
+app.use(express.json())
 
 let phoneNumbers = [
     { 
@@ -30,17 +32,8 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = parseInt(request.params.id)
-  console.log(phoneNumbers.entries)
-  const person = phoneNumbers.find(person => {
-    person.id === id
-    console.log(person.id === id)
-    // console.log("Person: " + person.id)
-    // console.log("ID: " + id)
-    // console.log(person.id == id)
-    // console.log(typeof(id))
-  })
-  console.log(person)
+  const id = Number(request.params.id)
+  let person = phoneNumbers.find(person => person.id === id)
 
   if (person) {
     response.json(person)
@@ -53,6 +46,31 @@ app.get('/info', (request, response) => {
     response.send(`
         <p>Phonebook has info for ${phoneNumbers.length}</p>
         <p>${new Date()}</p>`)
+})
+
+app.post('/api/persons', (request, response) => {
+  const newPerson = request.body
+  const ids = phoneNumbers.map(person => person.id)
+  let randomID = Math.floor(Math.random() * 10000)
+
+  while(ids.includes(randomID)) {
+    randomID = Math.floor(Math.random() * 10000)
+  }
+
+  newPersonObject = {
+    "id" : randomID,
+    "name" : newPerson.name,
+    "number" : newPerson.number
+  }
+
+  phoneNumbers = phoneNumbers.concat(newPersonObject)
+  response.json(newPersonObject)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  phoneNumbers = phoneNumbers.filter(person => person.id != id)
+  response.status(204).end()
 })
 
 const PORT = 3001
